35d66e673804298152635974154ca0919b3d5762
skalinin <kalinin@sent.com>
2017-09-20 15:08:45 +0300

    val на данный момент не используется

--- dataset_scripts/5_train_test.py
+++ dataset_scripts/5_train_test.py
@@ -9,21 +9,18 @@ dir_txt = ['BornDigitalImages_new.txt', 'COCO_new.txt', 'FocusedSceneText_new.tx
 
 dir_train_txt = 'dir_train.txt'
 dir_test_txt = 'dir_test.txt'
-dir_val_txt = 'dir_val.txt'
 
 count = 0
 count_train = 0
 count_test = 0
-count_val = 0
 
 train_share = 0.8
-test_share = 0.1
-val_share = 1 - (train_share + test_share)
+test_share = 1 - train_share
 
 train_txt = open(os.path.join(dir_dataset, dir_train_txt), 'w')
 test_txt = open(os.path.join(dir_dataset, dir_test_txt), 'w')
-val_txt = open(os.path.join(dir_dataset, dir_val_txt), 'w')
 
+random.seed(1)
 for dataset in dir_txt:
 	dir_dataset_txt = os.path.join(dir_dataset, dataset)
 	print(dataset, '...')
@@ -37,19 +34,14 @@ for dataset in dir_txt:
 		if chance < train_share:
 			count_train += 1
 			train_txt.write(line + '\n')
-		elif chance < train_share + test_share:
+		else:
 			count_test += 1
 			test_txt.write(line + '\n')
-		else:
-			count_val += 1
-			val_txt.write(line + '\n')
 
 train_txt.close()
 test_txt.close()
-val_txt.close()
 
 print('Done')
 print('всего изображений:', count)
 print('train:', count_train)
 print('test', count_test)
-print('val', count_val)
